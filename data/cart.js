d1537eddaadb96d63473bcad948a733b568e3fe3
varadharaj21 <varadharaj212@gmail.com>
2026-07-01 21:47:33 +0530

    delete product from the cart by clicking delete btn

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
- export const cart =[
+ export let cart =[
   {
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity : 2
@@ -27,3 +27,20 @@
   }
   // console.log(matchingItem);
 }
+
+export function deleteItemFromCart(deleteItemId){
+  
+  const newCart=[];
+  cart.forEach ( (cartItem)=>{
+   if(deleteItemId !== cartItem.productId){
+    newCart.push(cartItem)
+
+   }
+
+  console.log(cartItem.productId)
+
+  })
+  // console.log(newCart)
+  //    console.log("this is new cart product id" + newCart.productId)
+  cart = newCart;
+};
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import {cart} from "../data/cart.js";
+import {cart, deleteItemFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
 
 let cartSummaryHTML = ``;
 cart.forEach ( (cartItem)=>{
@@ -13,7 +14,7 @@ cart.forEach ( (cartItem)=>{
         }
 
     });
-    console.log(matchingProduct); 
+    // console.log(matchingProduct); 
 
     cartSummaryHTML += `
     <div class="cart-item-container">
@@ -40,7 +41,8 @@ cart.forEach ( (cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link"
+                  data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -97,4 +99,15 @@ cart.forEach ( (cartItem)=>{
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
-console.log(cartSummaryHTML);
+// console.log(cartSummaryHTML);
+
+document.querySelectorAll(".js-delete-link")
+.forEach( (link)=>{
+  link.addEventListener( 'click', ()=>{
+    const deleteItemId = link.dataset.productId;
+    console.log("this is delete id " + deleteItemId);
+    deleteItemFromCart(deleteItemId);
+    // console.log(cart);
+  })
+
+})
